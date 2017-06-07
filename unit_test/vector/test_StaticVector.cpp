df042884504755dd5812134ff624547563f1602f
looke <52668057@qq.com>
2017-06-07 22:56:10 +0800

    Add Test Case

--- unit_test/vector/test_StaticVector.cpp
+++ unit_test/vector/test_StaticVector.cpp
@@ -8,6 +8,21 @@
 #include "..\gtest_src\gtest\gtest.h"
 #include "..\include\vector\static\StaticVector.h"
 
+/*
+ * 测试vector创建以及异常处理
+ */
+TEST(StaticVectorCreateTest, postive)
+{
+	EXPECT_NO_THROW(StaticVector(0));
+	EXPECT_NO_THROW(StaticVector(10));
+	EXPECT_NO_THROW(StaticVector(20));
+	EXPECT_THROW(StaticVector(21), length_error);
+	EXPECT_THROW(StaticVector(-1), length_error);
+}
+
+/*
+ * 测试vector长度信息获取
+ */
 TEST(StaticVectorSizeTest, postive)
 {
 	StaticVector testVector = StaticVector(3);
@@ -17,6 +32,9 @@ TEST(StaticVectorSizeTest, postive)
 	EXPECT_EQ(13, testVector2.getDimension());
 }
 
+/*
+ * 测试vector元素查询
+ */
 TEST(StaticVectorGetElementTest, postive)
 {
 	StaticVector testVector = StaticVector(3);
@@ -24,6 +42,19 @@ TEST(StaticVectorGetElementTest, postive)
 	EXPECT_EQ(1, testVector.getElement(1));
 	EXPECT_EQ(1, testVector.getElement(2));
 
-	//
-	EXPECT_EQ(1, testVector.getElement(3));
 }
+
+/*
+ * 测试vector元素查询 异常处理
+ */
+TEST(StaticVectorGetElementExceptionTest, postive)
+{
+	StaticVector testVector = StaticVector(5);
+	//测试异常
+	EXPECT_THROW(testVector.getElement(5), length_error);
+	//测试异常
+	EXPECT_THROW(testVector.getElement(-1), length_error);
+	//测试异常
+	EXPECT_THROW(testVector.getElement(-3), length_error);
+}
+
